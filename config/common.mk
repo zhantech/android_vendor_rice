9ffe0ed2da1936194ff3340e6128c51d4b07c46f
Danny Lin <danny@kdrag0n.dev>
2022-09-24 06:40:07 +0800

    vendor: Import font modifications from protonAOSP
    Signed-off-by: str-br <alexfinhart@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -3,6 +3,7 @@ $(call inherit-product-if-exists, vendor/extra/product.mk)
 $(call inherit-product-if-exists, vendor/addons/config.mk)
 $(call inherit-product-if-exists, vendor/lineage/config/rice.mk)
 $(call inherit-product-if-exists, vendor/lineage/audio/audio.mk)
+$(call inherit-product-if-exists, vendor/lineage/fonts/fonts.mk)
 
 PRODUCT_BRAND ?= riceDroid
 
